0647f235a8ece8892e7f94026f0bd4a9d12dd953
abhishek18124 <abhishek18124@iiitd.ac.in>
2023-07-15 13:08:39 +0530

    Lecture 24

---
+++ Lecture 24/004CoinToss.cpp
@@ -0,0 +1,45 @@
+// n <= 10
+
+#include<iostream>
+
+using namespace std;
+
+void f(int n, char* out, int i, int j) {
+
+	// base case
+
+	if (i == n) {
+		// you've taken decisions for all the n coins
+		out[j] = '\0';
+		cout << out << endl;
+		return;
+	}
+
+	// recursive case
+
+	// take decisions for the remaining n-i coins starting from the ith coin
+
+	// for the ith coin, we've two options
+
+	// option 1 : ith coin shows head
+
+	out[j] = 'H';
+	f(n, out, i + 1, j + 1);
+
+	// option 2 : ith coin shows tail
+
+	out[j] = 'T';
+	f(n, out, i + 1, j + 1);
+
+}
+
+int main() {
+
+	int n = 3;
+
+	char out[10];
+
+	f(n, out, 0, 0);
+
+	return 0;
+}
